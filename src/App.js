9c90d1114253fdefe90dc3061f2fff0ccf78bcdd
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-12-08 23:10:50 +0900

    sadfasd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import "./styles.css";
 import { Header, Footer } from "./layouts/index";
 import Router from "./routes/Router";
-import { useStyle } from "@material-ui/styles";
 
 export default function App() {
-  const style = useStyle();
   return (
     <div className="App">
       <Header />
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <Provider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   rootElement
 );
