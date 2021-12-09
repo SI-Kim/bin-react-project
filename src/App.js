065f8f465a4c2ac2af535284b73a9ef326d7e55b
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-12-09 22:45:17 +0900

    sdaf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Header, Footer } from "./layouts/index";
 import Router from "./routes/Router";
+import { makeStyles } from "@mui/styles";
 
 export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.App}>
       <Header />
       <Router />
       <Footer />
     </div>
   );
 }
+
+const useStyles = makeStyles({
+  App: {
+    width: "inherit",
+    height: "inherit"
+  }
+});
